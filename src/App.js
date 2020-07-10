0baee28c58f0f20ef9babca2085421f7bbef24b8
pranav peddi <41251294+pranavpeddi@users.noreply.github.com>
2020-07-10 13:29:30 +0530

    first commit;

--- src/App.js
+++ src/App.js
@@ -2,25 +2,24 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ToDo from "./pages/todo";
+import {Provider as ReduxProvider} from 'react-redux';
+import ConfigureStore from "./modules/store";
+const reduxStore = ConfigureStore(window.REDUX_INITIAL_DATA);
+class App extends React.Component{
+  render() {
+    return (
+      <ReduxProvider store={reduxStore}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">ToDo Redux app</h1>
+          </header>
+          <ToDo />
+        </div>
+      </ReduxProvider>
+    );
+  }
 }
 
 export default App;
